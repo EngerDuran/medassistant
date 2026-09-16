432061e15835d5d34521f25a37dbfc9765a96b86
Enger Duran <engerduran15@gmail.com>
2026-09-16 17:58:02 +0200

    chatResponse fundaments

--- src/main/java/com/example/medassistant/TestLlmCall.java
+++ src/main/java/com/example/medassistant/TestLlmCall.java
@@ -3,8 +3,12 @@ package com.example.medassistant;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 
 @Component
@@ -15,8 +19,30 @@ public class TestLlmCall implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception { //Test de prueba para ver si funciona la api-key
-        String response = chatModel.call("Que es la fiebre?");
-        System.out.println(response);
+
+        ChatResponse chatResponse = chatModel.call(new Prompt("Explicame en dos párrafos la cervicalgia"));
+
+        String content = Objects.requireNonNull(chatResponse.getResult()).getOutput().getText();
+        System.out.println("=== RESPUESTA ===");
+        System.out.println(content);
+        System.out.println("=== FIN RESPUESTA ===");
+
+        System.out.println("\n=== METADATA ===\n");
+
+        System.out.println("Modelo: " +
+                chatResponse.getMetadata().getModel());
+
+        System.out.println("Tokens de entrada: " +
+                chatResponse.getMetadata().getUsage().getPromptTokens());
+
+        System.out.println("Tokens de salida: " +
+                chatResponse.getMetadata().getUsage().getCompletionTokens());
+
+        System.out.println("Tokens totales: " +
+                chatResponse.getMetadata().getUsage().getTotalTokens());
+
+        System.out.println("Finish reason: " +
+                chatResponse.getResult().getMetadata().getFinishReason());
 
     }
 }
